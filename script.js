36225d0910f09fc5e3ad1d112b4955c245fd1c84
hadespm <106915902+hadespm@users.noreply.github.com>
2026-06-29 03:34:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function copyPageLink() {
     navigator.clipboard.writeText(window.location.href);
 
-    alert("Đã sao chép liên kết!");
+    const toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 2000);
 }
