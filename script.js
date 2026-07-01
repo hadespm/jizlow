3662037bc60a8f86896c6a12e88613825e1582d2
hadespm <106915902+hadespm@users.noreply.github.com>
2026-07-01 15:29:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,17 @@ if(card){
     });
 
 }
+const lastUpdate = document.getElementById("lastUpdate");
+
+if(lastUpdate){
+
+    const today = new Date();
+
+    const date =
+        today.getDate().toString().padStart(2,"0") + "/" +
+        (today.getMonth()+1).toString().padStart(2,"0") + "/" +
+        today.getFullYear();
+
+    lastUpdate.textContent = "Cập nhật: " + date;
+
+}
